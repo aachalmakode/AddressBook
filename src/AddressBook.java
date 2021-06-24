d81ebe40f2eb560b7ac25c7f470aeadf4437bcd4
aachalmakode <aachalmakode900@gmail.com>
2021-06-24 19:57:47 -0400

    Adding Contact Details

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class AddressBook {
+    public ArrayList<ContactDetails> contactList = new ArrayList<>();
+
+    public  ArrayList<ContactDetails> contactList(ContactDetails cd){
+        contactList.add(cd);
+        return contactList;
+    }
 }
